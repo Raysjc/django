b566d88c27e8744da650e298ca5a794fdd5c554b
Raysjc <53925118+Raysjc@users.noreply.github.com>
2020-01-14 17:02:02 -0800

    Add files via upload

---
+++ pyhton2/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from tastypie.resources import ModelResource, fields, ALL
+from rental.models import Movie, Genre
+
+# Create your models here.
+class MovieResource(ModelResource):
+    class Meta:
+        queryset = Movie.objects.all()
+        resource_name = "movies"
+        filtering = {
+            'price': ALL,
+            'title': ALL,
+            'release_year': ALL
+        }
+        ordering: ['release_year', 'price', 'title', 'director']
+
+"""
+filtering:
+
+?price=20
+
+?price__gt=10
+?price__lt=30
+
+?title__contains=word
+"""
